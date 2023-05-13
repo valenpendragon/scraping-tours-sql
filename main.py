f6ee6067c968139e1635d41ed23d576ac636116e
Jeffrey Scott <valenpendragon@gmail.com>
2023-05-13 17:06:13 -0400

    Move data to SQLite database and update functions accordingly.

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import selectorlib
 import smtplib
 import os
 import ssl
+import sqlite3
+from pathlib import Path
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 # Note: Sometimes, we need a header to make it appear that the scraping function
 # is actually a browser instead of a scraping bot. requests.get has an optional
 # argument, headers. An example header string is stored in the extras directory.
+
+DATABASE = Path("G:\\Users\\valen\\Documents\\Valen\\python\\python-mega-course\\scraping-tours-sql\\data\\data.db")
+connection = sqlite3.connect(DATABASE)
 
 
 def scrape(url):
@@ -48,36 +53,54 @@ def send_email(message):
         server.sendmail(username, receiver, message)
 
 
-def store(extracted, datafile="./data/data.txt"):
+def store(extracted):
     """
-    This function adds the most recent concert to the datafile.
-    :param extracted:
+    This function adds the most recent concert tour data to the database.
+
+    Before making the database query, it will also clean up parts of the data
+    strings to eliminate trailing and leading blank spaces.
+    :param extracted: str
     :return:
     """
-    with open(datafile, "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
-def read_data(datafile="./data/data.txt"):
+def read_data(extracted):
     """
-    This function reads the lines in the datafile and returns them
-    as a list.
-    :param datafile: str, optional
-    :return: list of str
+    This function reads the lines in the datafile and returns a string of the
+    row, if any, in the database matching the extracted tour. An empty list
+    indicates that there are no matching entries in the database.
+
+    Before making the database query, it will also clean up parts of the data
+    strings to eliminate trailing and leading blank spaces.
+    :param database: Path, optional
+    :return: list of tuple or empty list
     """
-    with open(datafile, "r") as file:
-        content = file.readlines()
-    content = [item.replace("\n", "") for item in content]
-    return content
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                   (band, city, date))
+    rows = cursor.fetchall()
+    print(f"rows: {rows}")
+    return rows
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
-    data = read_data()
-    print(data)
+
     if extracted != "No upcoming tours":
-        if extracted not in data:
+        data = read_data(extracted)
+        print(data)
+        if not data:
             send_email(message="New event was found.")
+            print("Email sent.")
             store(extracted)
+            print(f"{extracted} written to database.")
